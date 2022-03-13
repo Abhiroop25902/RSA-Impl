ec1b497fd48b05312b882cef40dd5ccc0036e798
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2022-03-13 12:38:53 +0530

    [ADD] added different key encryption decryption

--- bin/rsa_impl.dart
+++ bin/rsa_impl.dart
@@ -13,32 +13,39 @@ void generateNewKeys() {
   var p = utils.generateLargePrime(utils.keySize);
   var q = utils.generateLargePrime(utils.keySize);
 
-  print('');
-  print('Generating primes...');
-  print('p: $p');
-  print('q: $q');
+  // print('');
+  // print('Generating primes...');
+  // print('p: $p');
+  // print('q: $q');
 
   var res = utils.generateKeys(p, q);
   e = res[0];
   d = res[1];
   n = res[2];
   print('Keys Generated!!');
+  showPublicKeys();
 }
 
-void showKeys() {
+void showPublicKeys() {
   print('');
   print('Public key: ');
   print('e: $e');
   print('n: $n');
-  print('Private key: ');
-  print('d: $d');
-  print('n: $n');
+  // print('Private key: ');
+  // print('d: $d');
+  // print('n: $n');
 }
 
 void encryptMessage() {
   stdout.write('Message: ');
   var m = utils.str2BigInt(stdin.readLineSync()!);
-  var c = encrypt.encryptRsa(m, e, n);
+  print('Enter Public Key of Recipient');
+  stdout.write('Enter e: ');
+  var recpE = BigInt.parse(stdin.readLineSync()!);
+  stdout.write('Enter n: ');
+  var recpN = BigInt.parse(stdin.readLineSync()!);
+  print('e: $e');
+  var c = encrypt.encryptRsa(m, recpE, recpN);
   print('Encrypted message: $c');
 }
 
@@ -58,7 +65,7 @@ void main(List<String> arguments) {
     print('└──────────────────────────────────────────────────────────────┘');
     print('Choose an option:');
     print('1. Generate new keys');
-    print('2. Show public and private keys');
+    print('2. Show public keys');
     print('3. Encrypt a message');
     print('4. Decrypt a message');
     print('5. Exit');
@@ -68,7 +75,7 @@ void main(List<String> arguments) {
     if (option == 1) {
       generateNewKeys();
     } else if (option == 2) {
-      showKeys();
+      showPublicKeys();
     } else if (option == 3) {
       encryptMessage();
     } else if (option == 4) {
